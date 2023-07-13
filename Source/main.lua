cf9a9aaaa20245be5f389fae39942f4d51a035a0
Basuke Suzuki <basuke@mac.com>
2023-07-13 07:58:51 -0700

    modified

--- Source/main.lua
+++ Source/main.lua
@@ -1,4 +1,46 @@
 require "simple"
 
 maze = generateMaze(10, 10)
+
+function printMaza(maze)
+    function printLine(cells, dir)
+        local line = ""
+        for x = 1, #cells do
+            cell = cells[x]
+            if (cell[dir]) then
+                line = line .. "####"
+            else
+                line = line .. "#  #"
+            end
+        end
+        print(line)
+    end
+
+    for y = 1, #maze do
+        local cells = maze[y]
+
+        printLine(cells, "N")
+        local line = ""
+        for x = 1, #cells do
+            cell = cells[x]
+            if (cell.W) then
+                line = line .. "#"
+            else
+                line = line .. " "
+            end
+            line = line .. "  "
+            if (cell.E) then
+                line = line .. "#"
+            else
+                line = line .. " "
+            end
+        end
+        print(line)
+
+        if (y == #maze) then
+            printLine(cells, "S")
+        end
+    end
+end
+
 printMaza(maze)
